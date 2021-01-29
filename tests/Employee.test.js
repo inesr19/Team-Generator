de656b8536bd094e29ddb8fa27e1c3f8f9bd301a
Ines Radic <inesradic12314@gmail.com>
2021-01-28 23:10:40 -0800

    update employee test

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,6 +1,6 @@
 const Employee = require('../lib/Employee');
 
-test("render new employee (object)", () => {
+test("render new employee object", () => {
   const employee = new Employee();
   expect(typeof(employee)).toBe("object");
 });
