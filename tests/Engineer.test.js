4451fea229cda4263f2e96964acff2375494d9aa
Ines Radic <inesradic12314@gmail.com>
2021-01-28 22:53:37 -0800

    add extends employee and test

--- tests/Engineer.test.js
+++ tests/Engineer.test.js
@@ -1,4 +1,14 @@
 const Employee = require('../lib/Employee');
 const Engineer = require('../lib/Engineer');
 
-test()
+test("Sets employee role as 'Engineer", () => {
+    const valueRole = 'Engineer';
+    const employee = new Engineer("Ines", 1, "awesome@gmail.com", valueRole);
+  expect(employee.getRole()).toBe(valueRole);
+});
+
+test('Generates github username', () => {
+    const valueUsername = 'inesr19'
+    const employee = new Engineer("Ines", 1, "awesome@gmail.com", valueUsername);
+    expect(employee.getGithub()).toBe(valueUsername);
+});
--- tests/Intern.test.js
+++ tests/Intern.test.js
@@ -1,2 +1,14 @@
 const Employee = require('../lib/Employee');
 const Intern = require('../lib/Intern');
+
+test("Sets employee role as 'Intern", () => {
+    const valueRole = 'Intern';
+    const employee = new Intern("Ines", 1, "awesome@gmail.com", valueRole);
+  expect(employee.getRole()).toBe(valueRole);
+});
+
+test('Return school name (string)', () => {
+    const valueSchool = 'UofO'
+    const employee = new Intern("Ines", 1, "awesome@gmail.com", valueSchool);
+  expect(employee.getSchool()).toBe(valueSchool);
+});
