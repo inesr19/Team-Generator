002d61b64497c19b1be71ca1c6dd9e296918b816
Ines Radic <inesradic12314@gmail.com>
2021-01-28 18:19:44 -0800

    add methods and constructor

--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -1,8 +1,8 @@
 const Manager = require("../lib/Manager");
 const Employee = require("../lib/Employee");
 
-test("Can set office number via constructor argument", () => {
-  const testValue = 100;
-  const e = new Manager("Foo", 1, "test@test.com", testValue);
-  expect(e.officeNumber).toBe(testValue);
+test("Sets the manager's office number", () => {
+  const value = 1;
+  const employee = new Manager("Ines", 1, "awesome@gmail.com", value);
+  expect(employee.officeNumber).toBe(value);
 });
